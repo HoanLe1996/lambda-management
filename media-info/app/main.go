6f04a91c553e5c408960b79e250f1506efcc5c17
HoanLV <tuibui1111@gmail.com>
2025-06-18 22:54:44 +0700

    Update greeting messages in lambda functions for clarity

--- media-info/app/main.go
+++ media-info/app/main.go
@@ -11,7 +11,7 @@ import (
 func handler(ctx context.Context, event events.S3Event) (finished bool, err error) {
 
 	fmt.Printf("----- start -----\n")
-	println("Hello World From lambda! Media")
+	println("Hello World From lambda! Media This is my test")
 	return true, nil
 }
 
